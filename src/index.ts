476562a8765bc7b023911c5f07edf9e87fa1a27f
Conny <conradkersten@gmail.com>
2024-06-13 21:17:00 -0500

    Get a sorta working idea together

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,21 @@ const body = {
     "content": "Here is a bunch of content. We should have used lorem ipsm",
     "authorEmail": "alice@prisma.io"
 }
+const EXPECTED_RESULT = 1;
+
+type result = {
+	initialDbVal: number;
+	resultStatusCode: number;
+	errorMessage: string | undefined;
+	resultDbVal: number;
+};
+
+let resultObj: result = {
+	initialDbVal: 0,
+	resultStatusCode: 0,
+	errorMessage: undefined,
+	resultDbVal: 0
+};
 
 function createDbConn(){
 	const db = new sqlite3.Database(filepath);
@@ -15,17 +30,33 @@ function createDbConn(){
 }
 
 function cbf(err: any, row: any){
+	console.log(1, err, row);
 	if (err) {
-		console.error('THERE WAS A PROBLEM');
+		resultObj.errorMessage = err.message;
 		return;
 	}
-	console.log(row.viewCount);
+	resultObj.initialDbVal = row.viewCount;
+	return;
+}
+
+function cbf2(err: any, row: any){
+	console.log(2, err, row);
+	if (err) {
+		resultObj.errorMessage = err.message;
+		return;
+	}
+	resultObj.resultDbVal = row.viewCount;
 	return;
 }
 
 function getValues(db: sqlite3.Database) {
 	db.get('SELECT * FROM Post'
 					, [], cbf);
+}
+
+function getValues2(db: sqlite3.Database) {
+	db.get('SELECT * FROM Post'
+					, [], cbf2);
 }
 
 let d = createDbConn();
@@ -37,8 +68,24 @@ const res = await fetch(url, {
 	headers: {'Content-Type':'application/json'}
 });
 
-console.log(res.status);
+resultObj.resultStatusCode = res.status;
 
 let d2 = createDbConn();
-getValues(d2);
+getValues2(d2);
 d2.close();
+
+function checkResult(){
+	setTimeout(() => {
+
+	if (resultObj.errorMessage) {
+		console.log('ERROR');
+		return;
+	}
+	console.log(resultObj);
+	let finalResult = resultObj.resultDbVal - resultObj.initialDbVal == EXPECTED_RESULT ?
+		'SUCCESS' : 'FAIL';
+	console.log(finalResult);
+}, 100);
+}
+
+checkResult();
